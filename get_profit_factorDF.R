bc4d17290e31d68be5f6061fca533a82b597e613
silkflo <florian.assous@hotmail.fr>
2020-10-20 23:07:32 +0700

    Update get_profit_factorDF.R
    change column name to keep it right with the other scripts

--- get_profit_factorDF.R
+++ get_profit_factorDF.R
@@ -12,38 +12,38 @@
 #' @examples
 get_profit_factorDF <- function(x, num_orders){
   # generate DF with only MagicNumbers when > 10 trades and all trades are losers
-  # x <- read_csv("C:/Program Files (x86)/AM MT4 - Terminal 2/tester/files/OrdersResultsT2.csv", col_names = F)
+  # x <- read_csv("C:/Program Files (x86)/AM MT4 - Terminal 2/tester/files/OrdersResultsT2.csv",  col_names = c("MagicNumber", "TicketNumber", "OrderStartTime", "OrderCloseTime", "Profit", "Symbol", "OrderType"), col_types = "iiccdci")
   # num_orders <- 10
   DF_L <- x %>%
-    group_by(X1) %>%
+    group_by(MagicNumber) %>%
     summarise(nOrders = n())%>%
     filter(nOrders > num_orders)%>%
-    select(X1)%>%
+    select(MagicNumber)%>%
     # subset only rows that contans magic numbers from x
-    inner_join(x, by = "X1")%>%
-    group_by(X1)%>%
-    filter(X5 < 0) %>%
-    summarise(Loss = abs(sum(X5)))
+    inner_join(x, by = "MagicNumber")%>%
+    group_by(MagicNumber)%>%
+    filter(Profit < 0) %>%
+    summarise(Loss = abs(sum(Profit)))
   # generate DF with only MagicNumbers when > 10 trades and all trades are profits
   DF_P <- x %>%
-    group_by(X1) %>%
+    group_by(MagicNumber) %>%
     summarise(nOrders = n())%>%
     filter(nOrders > num_orders)%>%
-    select(X1)%>%
+    select(MagicNumber)%>%
     # subset only rows that contans magic numbers from x
-    inner_join(x, by = "X1")%>%
-    group_by(X1)%>%
-    filter(X5 > 0) %>%
-    summarise(Gain = abs(sum(X5)))
+    inner_join(x, by = "MagicNumber")%>%
+    group_by(MagicNumber)%>%
+    filter(Profit > 0) %>%
+    summarise(Gain = abs(sum(Profit)))
   # generate DF containing profit factor of all systems
   DF <- DF_P %>%
-    full_join(DF_L, by = "X1")
+    full_join(DF_L, by = "MagicNumber")
   # replace any NA with 1!
   DF[is.na(DF)] <- 1
   # calculate profit factors of the each system!
   DF_PF <- DF%>%
-    group_by(X1)%>%
+    group_by(MagicNumber)%>%
     mutate(PrFact = Gain/(0.001+Loss))%>%
-    select(X1, PrFact)
+    select(MagicNumber, PrFact)
   return(DF_PF)
 }
